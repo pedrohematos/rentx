28dc9d9302e0acb525a663a4a21cab5ed2b1faf0
Pedro Matos <pedrohematos@outlook.com>
2021-09-26 21:07:39 -0300

    create category and specification routes

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,15 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+import { specificationsRoutes } from "./routes/specifications.routes";
+
 const app = express();
 const port = process.env.PORT || 3000; // Port where the application will run.
+
+app.use(express.json());
+
+app.use("/categories", categoriesRoutes);
+app.use("/specifications", specificationsRoutes);
 
 app.get("/", (req, res) => {
   return res.json({ message: "rentalx app" });
