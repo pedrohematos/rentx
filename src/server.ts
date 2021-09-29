b576996e488cc80c4967d346e82eb6ef43720a94
Pedro Matos <pedrohematos@outlook.com>
2021-09-28 23:09:16 -0300

    start docker

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 const port = process.env.PORT || 3000; // Port where the application will run.
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(express.json());
-
 app.use(router);
 
 app.listen(port, () => {
-  console.log(`Server succesfully running at http://localhost:${port}`);
+  console.log(`🔥 Server succesfully running at http://localhost:${port}`);
 });
